4940cdaab4505f57273fa885c2a127bc3663975f
Chamil Kandamby <chamil.kandamby@gmail.com>
2019-09-05 05:30:30 +0530

    onesignal push config

--- web-service/resources/views/cover_page.php
+++ web-service/resources/views/cover_page.php
@@ -51,5 +51,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="js/popper.min.js"></script>
+
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+    var OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+        OneSignal.init({
+            appId: "6f9e1a0e-5b77-488c-919c-12ad70ee1e10",
+        });
+    });
+</script>
 </body>
 </html>
